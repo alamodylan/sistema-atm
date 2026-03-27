a41f0e2aac52b831ddf7de018fe1fc895fb953ab
Dylan Bustos Artavia <dylansba777@gmail.com>
2026-03-27 13:25:51 -0600

    2

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,18 +1,34 @@
 from .article import Article
 from .audit_log import AuditLog
 from .deletion_request import WorkOrderLineDeleteRequest
+from .equipment import Equipment
+from .inventory import InventoryLedger, WarehouseStock, WarehouseLocationStock
 from .item_category import ItemCategory
-from .inventory import InventoryLedger, WarehouseStock
+from .notification import Notification
+from .physical_inventory import PhysicalInventory
+from .physical_inventory_line import PhysicalInventoryLine
 from .role import Role
+from .site import Site
+from .tool_loan import ToolLoan
+from .toolbox import Toolbox
+from .transfer import Transfer
+from .transfer_event import TransferEvent
+from .transfer_line import TransferLine
+from .transfer_request import TransferRequest
+from .transfer_request_line import TransferRequestLine
 from .unit import Unit
 from .user import User
+from .user_site_access import UserSiteAccess
+from .user_warehouse_access import UserWarehouseAccess
 from .warehouse import Warehouse
+from .warehouse_location import WarehouseLocation
 from .waste_act import WasteAct
 from .waste_act_line import WasteActLine
 from .work_order import WorkOrder, work_order_mechanics
 from .work_order_line import WorkOrderLine
 from .work_order_request import WorkOrderRequest
 from .work_order_request_line import WorkOrderRequestLine
+from .work_order_service import WorkOrderService
 
 __all__ = [
     "Role",
@@ -20,6 +36,7 @@ __all__ = [
     "AuditLog",
     "Article",
     "Warehouse",
+    "WarehouseLocation",
     "InventoryLedger",
     "WarehouseStock",
     "WarehouseLocationStock",
@@ -33,4 +50,25 @@ __all__ = [
     "Unit",
     "WorkOrderRequest",
     "WorkOrderRequestLine",
+
+    # nuevos
+    "Site",
+    "Equipment",
+    "ToolLoan",
+    "Toolbox",
+    "WorkOrderService",
+
+    "TransferRequest",
+    "TransferRequestLine",
+    "Transfer",
+    "TransferLine",
+    "TransferEvent",
+
+    "PhysicalInventory",
+    "PhysicalInventoryLine",
+
+    "Notification",
+
+    "UserSiteAccess",
+    "UserWarehouseAccess",
 ]
