cb511bb0aee0043b09579b8f0b74acfdec6fce77
Dylan Bustos Artavia <dylansba777@gmail.com>
2026-05-19 10:29:40 -0600

    6

--- app/models/purchase_request_line.py
+++ app/models/purchase_request_line.py
@@ -74,18 +74,6 @@ class PurchaseRequestLine(db.Model):
         server_default=db.func.now(),
     )
 
-    review_site_id = db.Column(
-        db.BigInteger,
-        db.ForeignKey("atm.sites.id"),
-        nullable=True,
-    )
-
-    sent_direct_to_procurement = db.Column(
-        db.Boolean,
-        nullable=False,
-        default=False,
-    )
-
     purchase_request = db.relationship(
         "PurchaseRequest",
         back_populates="lines",
@@ -110,11 +98,6 @@ class PurchaseRequestLine(db.Model):
         "PurchaseOrderLine",
         back_populates="purchase_request_line",
         lazy="selectin",
-    )
-
-    review_site = db.relationship(
-        "Site",
-        foreign_keys=[review_site_id],
     )
 
     @property
