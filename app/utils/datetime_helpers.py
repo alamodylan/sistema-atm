81b444e1929e9c01e746f0ef032af0c13c068b34
Dylan Bustos Artavia <dylansba777@gmail.com>
2026-08-04 12:45:24 -0600

    33

--- app/utils/datetime_helpers.py
+++ app/utils/datetime_helpers.py
@@ -1,4 +1,4 @@
-from datetime import UTC
+from datetime import UTC, date, datetime
 from zoneinfo import ZoneInfo
 
 
@@ -9,16 +9,27 @@ def to_costa_rica_time(value):
     if not value:
         return None
 
+    # Si ya es solo una fecha, se devuelve igual
+    if isinstance(value, date) and not isinstance(value, datetime):
+        return value
+
     if value.tzinfo is None:
         value = value.replace(tzinfo=UTC)
 
     return value.astimezone(COSTA_RICA_TZ)
 
 
-def format_costa_rica_datetime(value, fmt="%d/%m/%Y %H:%M"):
+def format_costa_rica_datetime(
+    value,
+    fmt="%d/%m/%Y %H:%M",
+):
     local_value = to_costa_rica_time(value)
 
     if not local_value:
         return "-"
+
+    # Si es únicamente una fecha
+    if isinstance(local_value, date) and not isinstance(local_value, datetime):
+        return local_value.strftime("%d/%m/%Y")
 
     return local_value.strftime(fmt)
